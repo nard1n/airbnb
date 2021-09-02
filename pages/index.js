a3776d6ea7f539066d62ad3238bf717ac440c71c
Nardin Lachowski <nardin@live.com>
2021-09-02 15:53:58 -0400

    starter next tw react app files

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      {/* Banner */}
+      {/* Body */}
+      {/* Footer */}
+
+    </div>
+  )
+}
