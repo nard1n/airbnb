f5478d79a8034992b927c41792c49010a0a22f31
Nardin Lachowski <nardin@live.com>
2021-09-03 07:03:31 -0400

    added styling to the medium component on the page with flex and overflow scroll

--- pages/index.js
+++ pages/index.js
@@ -33,14 +33,15 @@ export default function Home({ exploreData, cardsData }) {
         </section>
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-
-          {cardsData.map( ({img, title}) => (
-                <MediumCard
-                  key={img} 
-                  img={img}
-                  title={title}
-                />
-              ))}
+          <div className="flex space-x-3 overflow-scroll">
+            {cardsData.map( ({img, title}) => (
+                  <MediumCard
+                    key={img} 
+                    img={img}
+                    title={title}
+                  />
+                ))}
+          </div>
         </section>
 
         {/* Footer */}
