532ad987f9c8ff66ce29568ec575ee8a8796f8e3
Nardin Lachowski <nardin@live.com>
2021-09-03 06:31:46 -0400

    importing of smallcard component and added async functoin to get details for the cards from server

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import SmallCard from '../components/SmallCard';
 
-export default function Home() {
+export default function Home({ exploreData }) {
+  exploreData
+  
   return (
     <div className="">
       <Head>
@@ -15,12 +18,35 @@ export default function Home() {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {/* Pulling card images from server - static rendering here */}
+              {exploreData?.map( ({img, distance, location}) => (
+                <SmallCard
+                  key={img} 
+                  img={img}
+                  distance={distance}
+                  location={location}
+                />
+              ))}
+            </div>
         </section>
-        {/* Body */}
+        
         {/* Footer */}
       </main>
 
 
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp')
+  .then(res => res.json()
+  );
+
+  return {
+    props: {
+      exploreData
+    }
+  }
 }
