d90725fae451554e7244305403468da57ed948a1
Nardin Lachowski <nardin@live.com>
2021-09-05 09:32:30 -0400

    completed the footer component with the different footer link texts

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14  bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-sx text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-sx text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is ot a real site</p>
+                <p>Just a clone</p>
+                <p>Airbnb is awesome</p>
+                <p>Say hello!</p>
+            </div>
+            <div className="space-y-4 text-sx text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Nardin</p>
+                <p>Presents</p>
+                <p>Clone</p>
+                <p>Open Source </p>
+                <p>Join me</p>
+            </div>
+            <div className="space-y-4 text-sx text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help center</p>
+                <p>Trust & Safety</p>
+                <p>@nard1n on Github</p>
+                <p>Tailwaind and Next.js</p>
+                <p>Sweet clone</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
