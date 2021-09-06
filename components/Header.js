ea361fb0f1297b335bedde3735868bae6d67a633
Nardin Lachowski <nardin@live.com>
2021-09-06 13:59:13 -0400

    iimplementing usestate hook to handle input field and selected date ranges

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "next/image";
 import { 
     SearchIcon,
@@ -7,10 +7,27 @@ import {
     UsersIcon,
     MenuIcon,
  } from '@heroicons/react/solid';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
+    const [searchInput, setSearchInput] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection'
+    }
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
     return (
-        <div className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             {/* Left */}
             <div className='relative flex items-center h-10 cursor-pointer my-auto'>
                 <Image 
@@ -23,7 +40,10 @@ function Header() {
 
             {/* Middle */}
             <div className="flex item-center md:border-2 md:shadow-sm rounded-full py-2">
-                <input className=" flex-grow pl-5 bg-transparent outline-none text-sm text-gray-400" type="text" placeholder="Start your search" />
+                <input 
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                className=" flex-grow pl-5 bg-transparent outline-none text-sm text-gray-400" type="text" placeholder="Start your search" />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer mx-auto md:mx-2" />
             </div>
             {/* Right */}
@@ -36,7 +56,17 @@ function Header() {
                     <UserCircleIcon className="h-6" />
                 </div>
             </div>
-        </div>
+            {searchInput && (
+                <div className="flex flex-col col-span-3 mx-auto">
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={["#FD5B61"]}
+                        onChange={handleSelect}
+                    />
+                </div>
+            )}
+        </header>
     )
 }
 
