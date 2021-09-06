0b12cc71913f1f7ce504f4dee46592a78f4edeb1
Nardin Lachowski <nardin@live.com>
2021-09-06 10:30:32 -0400

    moved placement of Footer to correct width

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Head from 'next/head';
-import Banner from '../components/Banner';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import LargeCard from '../components/LargeCard';
-import MediumCard from '../components/MediumCard';
-import SmallCard from '../components/SmallCard';
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import LargeCard from "../components/LargeCard";
+import MediumCard from "../components/MediumCard";
+import SmallCard from "../components/SmallCard";
 
 export default function Home({ exploreData, cardsData }) {
-  exploreData
-  
+  exploreData;
+
   return (
     <div className="">
       <Head>
@@ -23,26 +23,22 @@ export default function Home({ exploreData, cardsData }) {
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {/* Pulling card images from server - static rendering here */}
-              {exploreData?.map( ({img, distance, location}) => (
-                <SmallCard
-                  key={img} 
-                  img={img}
-                  distance={distance}
-                  location={location}
-                />
-              ))}
-            </div>
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
         </section>
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
-            {cardsData.map( ({img, title}) => (
-                  <MediumCard
-                    key={img} 
-                    img={img}
-                    title={title}
-                  />
-                ))}
+            {cardsData.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
           </div>
         </section>
         <LargeCard
@@ -51,28 +47,25 @@ export default function Home({ exploreData, cardsData }) {
           buttonText="Get Inspired"
           title="The Greatest Outdoors"
         />
-
-        <Footer />
       </main>
-
-
+      <Footer />
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const exploreData = await fetch('https://links.papareact.com/pyp')
-  .then(res => res.json()
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
   );
 
-  const cardsData = await fetch('https://links.papareact.com/zp1')
-  .then((res) => res.json()
+  const cardsData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
   );
 
   return {
     props: {
       exploreData,
       cardsData,
-    }
-  }
+    },
+  };
 }
