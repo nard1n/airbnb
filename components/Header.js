a0ccae09c632a54c2534336cc0e4d767dabfff2b
Nardin Lachowski <nardin@live.com>
2021-09-06 14:16:39 -0400

    added the number of input section for the num of guests with state set in place

--- components/Header.js
+++ components/Header.js
@@ -15,6 +15,7 @@ function Header() {
     const [searchInput, setSearchInput] = useState('');
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
+    const [noOfGuests, setNoOfGuests] = useState(1);
 
     const selectionRange = {
         startDate: startDate,
@@ -64,6 +65,21 @@ function Header() {
                         rangeColors={["#FD5B61"]}
                         onChange={handleSelect}
                     />
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className='text-2xl flex-grow font-semibold'>Number of Guests</h2>
+                        <UsersIcon className="h-5" />
+                        <input 
+                            value={noOfGuests}
+                            type="number"
+                            className="w-12 pl-2 text-lg outline-none text-red-400"
+                            onChange={(e) => setNoOfGuests(e.target.value)}
+                            min={1}
+                        />
+                    </div>
+                    <div className="flex flex-grow">
+                        <button className="flex-grow text-gray-500">Cancel</button>
+                        <button className="flex-grow text-red-400">Search</button>
+                    </div>
                 </div>
             )}
         </header>
