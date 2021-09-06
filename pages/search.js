4bb2885dee12a93d19608b08c22ad4634b6ff31d
Nardin Lachowski <nardin@live.com>
2021-09-06 15:31:22 -0400

    top section of the search page, with router, destructuring the search parameters to duse throughout page

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,37 @@
+import { format } from "date-fns";
+import { useRouter } from "next/dist/client/router";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function Search() {
+    const router = useRouter();
+    const {location, startDate, endDate, noOfGuests } = router.query;
+    const formattedSartDate = format(new Date(startDate), "dd MMMM yy");
+    const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+
+    const dateRange = `${formattedSartDate} - ${formattedEndDate}`
+
     return (
-        <div>
-            <Header />
+        <div className="">
+            <Header placeholder={`${location} | ${dateRange} | ${noOfGuests} guests`} />
+
+            <main className="flex">
+                <section className="flex-grow pt-14 px-6">
+                    <p className="text-xs">300+ stays - {dateRange} - {noOfGuests} guests</p>
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
+
+                    <div className="mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+                        <p className="button">Cancellation Flexibility</p>
+                        <p className="button">Type of Place</p>
+                        <p className="button">Price</p>
+                        <p className="button">Rooms and Beds</p>
+                        <p className="button">More filters</p>
+                    </div>
+                    
+                </section>
+            </main>
+
+            <Footer />
         </div>
     )
 }
