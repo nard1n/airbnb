19728e7e8ee87282a193a584e1f7bcb3aeb7e078
Nardin Lachowski <nardin@live.com>
2021-09-06 14:25:44 -0400

    importing the header nav in search page

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,9 @@
+import Header from "../components/Header";
+
 function Search() {
     return (
         <div>
-            <h1> I am the search page</h1>
+            <Header />
         </div>
     )
 }
