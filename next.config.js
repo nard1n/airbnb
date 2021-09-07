b7f390e03ed8915b6743b79c74791c75c895037e
Nardin Lachowski <nardin@live.com>
2021-09-07 12:34:26 -0400

    added mapbox public key

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
         domains: ["links.papareact.com"]
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1IjoibmFyZDFuIiwiYSI6ImNrdGEyeWs1ejFpNnQycG56ZXFvem8xeHIifQ.y2R2fIs6Ylv6uhtiPpmXtA'
     }
 }
