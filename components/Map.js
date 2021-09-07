1c9e9e613f759cd4019fe94d28574906aa09d100
Nardin Lachowski <nardin@live.com>
2021-09-07 14:01:20 -0400

    added popup and marker from react map gl, transformed lat log into array of object in order to center the map around the suggested results

--- components/Map.js
+++ components/Map.js
@@ -1,24 +1,66 @@
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
 
-function Map() {
-    const [viewport, setViewport] = useState({
-        width: '100%',
-        height: '100%',
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 11,        
-    });
+const Map = ({ searchResults }) => {
+  const [selectedLocation, setSelectedLocation] = useState({});
 
-    return (
-        <ReactMapGL
-            mapStyle='mapbox://styles/nard1n/ckta3bzdz1v2817mk40sme873'
-            mapboxApiAccessToken={process.env.mapbox_key}
-            {...viewport}
-        >
-            
-        </ReactMapGL>
-    )
-}
+  //   Transform coordinates into required array of objects in the correct shape
+  const coordinates = searchResults.map((result) => ({
+    latitude: result.lat,
+    longitude: result.long,
+  }));
 
-export default Map
+  // The latitude and longitude of the center of locations coordinates
+  const center = getCenter(coordinates);
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/nard1n/ckta3bzdz1v2817mk40sme873"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+        {searchResults.map((result) => (
+            <div key={result.long}>
+                <Marker
+                    longitude={result.long}
+                    latitude={result.lat}
+                    offsetLeft={-20}
+                    offsetTop={-10}
+                >
+                    <p 
+                    onClick={() => setSelectedLocation(result)}
+                    aria-lable="push-pin"
+                    role="img"
+                    className="cursor-pointer text-2xl animate-pulse">📌</p>
+                </Marker>
+
+                {/* popup that shows if we click on a maker */}
+                {selectedLocation.long === result.long ? (
+                    <Popup
+                        onClose={() => setSelectedLocation({})}
+                        closeOnClick={true}
+                        latitude={result.lat}
+                        longitude={result.long}
+                    >
+                        {result.title}
+                    </Popup>
+                ) : (
+                    false
+                )}
+            </div>
+        ))}
+    </ReactMapGL>
+  );
+};
+
+export default Map;
